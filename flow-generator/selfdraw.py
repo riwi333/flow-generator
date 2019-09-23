f12988fa0c7bf81ae16de7decfaa5d894e77eb8b
trifork3 <rwilliams8183@gmail.com>
2019-09-22 23:31:43 -0400

    updated selfdraw.py to reflect changes to Grid and Flow classes

--- flow-generator/selfdraw.py
+++ flow-generator/selfdraw.py
@@ -1,5 +1,5 @@
 import pyglet
-import graphics as graphics
+import graphics
 from grid import Grid
 from flow import Flow
 from random import random, seed
@@ -26,9 +26,6 @@ GRID_SIZE = 7
 # seed the random generator
 seed(datetime.now())
 
-# record of which flow (0-indexed) is covering each cell
-cells = [ [-1 for y in range(GRID_SIZE) ] for x in range(GRID_SIZE) ]
-
 # create the window and the grid
 window = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT)
 grid = Grid(    [ 50, 50 ],
@@ -44,25 +41,13 @@ n_flow = 0
 
 # set up the cursor
 flow_selected = False                       # boolean of whether or now we're currently drawing a flow
-cursor_cell = [ 0 , grid.rows - 1 ]         # start the cursor in the top left corner
+cursor_cell = ( 0 , grid.rows - 1 )         # start the cursor in the top left corner
 cursor_batch = pyglet.graphics.Batch()
 cursor = graphics.generateCircle(   grid.getCellCenter(cursor_cell),
                                     0.2 * min(*grid.getSpacing()),
                                     15,
                                     fill = True,
                                     batch = cursor_batch )
-
-# return the index of the flow at this cell
-def cellFlow(cell):
-    return cells[ cell[0] ][ cell[1] ]
-
-# boolean of whether this (col, row) tuple is in the grid
-def cellInGrid(cell, grid):
-    if 0 <= cell[0] < grid.cols:
-        if 0 <= cell[1] < grid.rows:
-            return True
-
-    return False
 
 # draw the grid, flows, and cursor on the window
 @window.event
@@ -77,58 +62,58 @@ def on_draw():
         flow.draw()
 
 # dictionary of arrow key mappings to coordinate changes
-keys = {    pyglet.window.key.LEFT: [ -1, 0 ],
-            pyglet.window.key.RIGHT: [ 1, 0 ],
-            pyglet.window.key.DOWN: [ 0, -1 ],
-            pyglet.window.key.UP: [ 0, 1 ]  }
+keys = {    pyglet.window.key.LEFT: ( -1, 0 ),
+            pyglet.window.key.RIGHT: ( 1, 0 ),
+            pyglet.window.key.DOWN: ( 0, -1 ),
+            pyglet.window.key.UP: ( 0, 1 )  }
 
 # control drawing with the keyboard
 @window.event
 def on_key_press(symbol, modifiers):
-    global flow_selected, flows, n_flow, cursor_cell, cursor, cells
+    global flow_selected, flows, n_flow, cursor_cell, cursor, grid
 
     next_cell = cursor_cell
 
-    print(cursor_cell, cellFlow(cursor_cell))
+    print(cursor_cell)
+    print(grid.values[cursor_cell])
+    print(flow_selected)
+    print("")
 
     # press the spacebar to start drawing a flow; press it again to stop
     if symbol == pyglet.window.key.SPACE:
         # if we're not currently drawing a flow, make a new one
-        if flow_selected is False and cellFlow(cursor_cell) == -1:
+        if flow_selected is False and grid.values[cursor_cell] == None:
             flows.append(Flow(  grid,
                                 [ floor(random() * 255) for x in range(3) ],
                                 n_flow  ))
 
             # make sure the initial endpoint is drawn
             flows[n_flow].addCell(cursor_cell)
-
-            cells[ cursor_cell[0] ][ cursor_cell[1] ] = n_flow
             flow_selected = True
 
         # if we're currently drawing a flow, put its second endpoint down and stop
-        elif flow_selected is True and cellFlow(cursor_cell) == n_flow:
-            flows[n_flow].addCell(cursor_cell)
+        elif flow_selected is True and grid.values[cursor_cell] == n_flow:
+                flows[n_flow].addCell(cursor_cell)
 
-            n_flow = n_flow + 1
-            flow_selected = False
+                n_flow = n_flow + 1
+                flow_selected = False
 
     # press the backspace key on a flow (or while drawing a flow) to delete it
-    elif symbol == pyglet.window.key.BACKSPACE and cellFlow(cursor_cell) > -1:
-        flow_index = cellFlow(cursor_cell)
+    elif symbol == pyglet.window.key.BACKSPACE and not grid.values[cursor_cell] == None:
+        flow_index = grid.values[cursor_cell]
 
         condition1 = flow_selected is True
         condition2 = flow_selected is False and flow_index > -1
 
         # delete either the current flow being drawn or a finished flow
         if condition1 or condition2:
-            # update the cells[] record of which flow belongs to each cell
-            for i in range(GRID_SIZE):
-                for j in range(GRID_SIZE):
-                    if cells[i][j] > flow_index:
-                        cells[i][j] = cells[i][j] - 1
+            # update the record of which flow belongs to each cell
+            for cell in grid.getAllCellCoordinates():
+                if not grid.isEmpty(cell) and grid.values[cell] > flow_index:
+                    grid.values[cell] = grid.values[cell] - 1
 
-                    if cells[i][j] == flow_index:
-                        cells[i][j] = -1
+                elif grid.values[cell] == flow_index:
+                    grid.values[cell] = None
 
             # delete the Flow object
             flow_object = flows.pop(flow_index)
@@ -142,18 +127,17 @@ def on_key_press(symbol, modifiers):
 
     # move the cursor and flows with the arrow keys
     elif symbol in keys.keys():
-        next_cell = [ cursor_cell[0] + keys[symbol][0], cursor_cell[1] + keys[symbol][1] ]
+        next_cell = ( cursor_cell[0] + keys[symbol][0], cursor_cell[1] + keys[symbol][1] )
 
         # if a flow is being drawn, add this next (empty, within the grid) cell to it
         if flow_selected is True:
-            if cellInGrid(next_cell, grid) and cellFlow(next_cell) == -1:
+            if grid.inBounds(next_cell) and grid.isEmpty(next_cell):
                 flows[n_flow].addCell(next_cell)
-                cells[ next_cell[0] ][ next_cell[1] ] = n_flow
             else:
                 next_cell = cursor_cell
 
         if flow_selected is False:
-            if not cellInGrid(next_cell, grid):
+            if not grid.inBounds(next_cell):
                 next_cell = cursor_cell
 
     # update the cursor's position on the grid

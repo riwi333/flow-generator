59548eabe049069a69a0e133d14fb8ab74b227be
trifork3 <rwilliams8183@gmail.com>
2019-10-14 03:30:44 -0400

    added current commit information and some formatting to performance evaluation script

--- tools/performance.py
+++ tools/performance.py
@@ -1,13 +1,28 @@
 from context import Grid, Flow, generator
 from time import process_time, gmtime
+import git
 
 """
 measure the success rates and average runtime of flow generation for various grid sizes
 """
 
 MIN_SIZE = 4        # minimum grid size to generate flows for
-MAX_SIZE = 13       # maximum grid size to generate flows for
-NUM_TESTS = 3      # number of flow generations to do per grid size
+MAX_SIZE = 15       # maximum grid size to generate flows for
+NUM_TESTS = 20      # number of flow generations to do per grid size
+
+# include current git master branch commit information to help track performance progress
+current_commit = git.Repo(search_parent_directories=True).heads.master.commit
+
+# figure out date and time via the count of seconds from epoch to the commit
+commit_time = current_commit.authored_date
+time_struct = gmtime(commit_time)
+month, day, year = time_struct.tm_mon, time_struct.tm_mday, time_struct.tm_year
+hour, minute, second = time_struct.tm_hour, time_struct.tm_min, time_struct.tm_sec
+datestring = str(month) + "/" + str(day) + "/" + str(year) + " " + str(hour) + ":" + str(minute) + ":" + str(second)
+
+print("Commit: " + str(current_commit.message.strip()))
+print(datestring)
+print("\n")
 
 # make a grid for each measured size (graphics-related sizing doesn't matter here)
 grids = [   Grid(   [0, 0],
@@ -36,9 +51,12 @@ for i in range(MAX_SIZE - MIN_SIZE + 1):
     success_rates.append(success)
     times.append(runtime)
 
+print("Size\tSuccess rate\tAverage runtime")
+
+# print out all the collected information
 for i in range(MAX_SIZE - MIN_SIZE + 1):
     success_rates[i] = float(success_rates[i]) / NUM_TESTS
     times[i] = times[i] / NUM_TESTS
-    print(str(i + MIN_SIZE) + "x" + str(i + MIN_SIZE) + ": ")
-    print("Success rate: " + str(success_rates[i]))
-    print("Average runtime: " + str(times[i]))
+    print(str(i + MIN_SIZE) + "x" + str(i + MIN_SIZE) + ":\t" + str(success_rates[i]) + "\t\t" + str(times[i]))
+
+print("\nPerformed " + str(NUM_TESTS) + " tests per size")

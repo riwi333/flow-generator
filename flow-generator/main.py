6413761cc2ed6eb1df816d183879223a9815669a
trifork3 <rwilliams8183@gmail.com>
2019-10-30 16:46:49 -0400

    added more functionality to main script

--- flow-generator/main.py
+++ flow-generator/main.py
@@ -3,23 +3,15 @@ from grid import Grid
 from flow import Flow
 import generator
 from math import floor
-from random import random
+from random import random, shuffle
 import sys
 
 """
 run the path generation algorithm and display the resulting solved flow puzzle
 
 TODO:
-    -   add option to see puzzle with or without the attached puzzles (see unsolved versions)
-
     -   (eventually) add option to play puzzle with controls similar to 'tools/selfdraw.py'
-
-    -   prevent darker/less-visible colors from being used, and don't repeat any colors
-        (easy way to do this is to choose a permanent set of 30ish colors, and have each
-        flow use one of the colors in the set w/o replacement)
-
-    -   randomize grid dimension size if no arguments are given
-
+    
 """
 
 WINDOW_WIDTH = 960
@@ -27,23 +19,67 @@ WINDOW_HEIGHT = 540
 GRID_ORIGIN = (50, 50)
 GRID_WIDTH = WINDOW_WIDTH - 2 * GRID_ORIGIN[0]
 GRID_HEIGHT = WINDOW_HEIGHT - 2 * GRID_ORIGIN[1]
+MAX_ROWS = 20
+MAX_COLS = 20
 
-DEFAULT_ROWS = 5
-DEFAULT_COLS = 5
+# list of distinct colors we use for flows
+COLORS = [
+    (0, 255, 0),        # green
+    (0, 0, 255),        # dark blue
+    (255, 0, 255),      # fuchsia
+    (153, 102, 51),     # brown
+    (255, 255, 0),      # yellow
+    (255, 153, 0),      # orange
+    (255, 0, 0),        # red
+    (153, 0, 255),      # purple
+    (255, 255, 255),    # white
+    (0, 255, 255),      # aqua blue
+    (153, 204, 255),    # baby blue
+    (153, 0, 0),        # dark red
+    (229, 204, 255),    # light purple
+    (255, 250, 200),    # beige
+    (230, 190, 138),    # rose gold
+    (255, 77, 136),     # rose
+    (59, 122, 87),      # dark green
+    (128, 128, 128),    # gray
+    (176, 191, 26),     # acid green
+    (70, 153, 144),     # teal
+    (0, 175, 255),      # sky blue
+    (95, 95, 255),      # royal blue
+    (135, 0, 135),      # magenta
+    (228, 228, 228)     # light gray
+]
+
+assert len(COLORS) >= max([ MAX_ROWS, MAX_COLS ]), "Not enough colors for all grids"
+
+# flag to decide whether the grid should be displayed solved or unsolved
+show_solved = True
+
+# get the option to see the grid unsolved (solved by default)
+if "--unsolved" in sys.argv:
+    sys.argv.remove("--unsolved")
+    show_solved = False
+
+if "--solved" in sys.argv:
+    sys.argv.remove("--solved")
+    show_solved = True
 
 # get the grid dimensions from the command line arguments
 try:
-    rows = int(sys.argv[1])
-    cols = int(sys.argv[2])
+    rows = min([ int(sys.argv[1]), MAX_ROWS ])
+    cols = min([ int(sys.argv[2]), MAX_COLS ])
 
 except ValueError:
-    print("Grid dimensions must be integers; using a 5x5 grid by default")
-    rows, cols = DEFAULT_ROWS, DEFAULT_COLS
+    rows = floor(random() * (MAX_ROWS - 4)) + 4
+    cols = min([ rows, MAX_COLS ])
+    print("Grid dimensions must be integers; generating a " + str(rows) + "x" + str(cols) + " grid instead")
+    cols = rows
 
 except IndexError:
     if len(sys.argv) == 1:
-        print("No dimensions provided; using a 5x5 grid by default")
-        rows, cols = DEFAULT_ROWS, DEFAULT_COLS
+        rows = floor(random() * (MAX_ROWS - 4)) + 4
+        cols = min([ rows, MAX_COLS ])
+        print("No dimensions provided; generating a " + str(rows) + "x" + str(cols) + " grid")
     else:
         cols = rows
 
@@ -71,12 +107,32 @@ paths = generator.simplifyPaths(grid, paths)
 flatten = lambda multi : [x for arr in multi for x in arr]
 assert len(flatten(paths)) == len(set(flatten(paths)))
 
+# make sure we have enough colors for this set of paths
+assert len(COLORS) >= len(paths), "Not enough colors for this grid"
+
+# shuffle the list of colors so random ones are chosen
+shuffle(COLORS)
+
 flows = []
 for path in paths:
-    flows.append(   Flow(   grid,
-                            [ floor(random() * 256) for x in range(3) ],
-                            len(flows),
-                            path = path   )  )
+    color = COLORS.pop()
+
+    # show the completed flows or just their endpoints based on the given flag
+    # (completed flows are shown by default)
+    if show_solved:
+        flows.append(   Flow(   grid,
+                                color,
+                                len(flows),
+                                path = path   )  )
+    else:
+        flows.append(   Flow(   grid,
+                                color,
+                                len(flows),
+                                path = [ path[0] ]  ) )
+        flows.append(   Flow(   grid,
+                                color,
+                                len(flows),
+                                path = [ path[-1] ]  ) )
 
 # update all the flows' graphics so they can be drawn
 for flow in flows:
